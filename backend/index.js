f14d7ebae8415afc00f740071f85ef8576cb534e
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-09 16:23:37 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 // Routes
 app.get('/final', async (req, res) => {
     try {
-        const memes = await Final.find();
+        const final = await Final.find();
         res.json(memes);
     } catch (err) {
         console.error(err);
