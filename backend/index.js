cc4ede734de093e66631190d88deccc89b8afe39
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-09 16:22:23 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const finalSchema = new mongoose.Schema({
     source: String
 });
 
-const Meme = mongoose.model('Meme', memeSchema);
+const Final = mongoose.model('Meme', memeSchema);
 
 app.use(bodyParser.json());
 
