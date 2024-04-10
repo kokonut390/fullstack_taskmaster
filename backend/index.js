e020d606ca0206207bda35f0769b37e81b2ab790
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-09 20:17:22 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://comp227:${password}@cluster0.rt7kzix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://comp227:${password}@cluster0.rt7kzix.mongodb.net/schedule?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.connect(url, {
     useNewUrlParser: true,
