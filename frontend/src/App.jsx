cb3b452bbddf5f7c2f46c4e1d932dc69ac75ae5f
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-09 22:47:59 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     useEffect(()=> {
         fetch('http://localhost:3001/schedule')
             .then(response => response.json())
-            .then()
+            .then(data =)
             .catch()
     })
   return (
