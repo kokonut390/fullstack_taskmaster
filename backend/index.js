f984020cd43ac8f3d4ed4aa0224a85e9ea3a3b57
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-16 22:30:49 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@ app.put('/schedule', async (req, res) => {
         if (updatedSchedule){
             res.json(updatedSchedule)
         }else {
-            res.status(404).json({me})
+            res.status(404).json({mess})
         }
     }
 })
