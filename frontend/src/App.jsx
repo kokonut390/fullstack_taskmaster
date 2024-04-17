b89854f4d0e22c8ad5384bcb6efa2f0fe194c7ab
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-16 22:42:35 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,8 @@ function App() {
             .then(response => response.json())
             .then(data => {
                 setSchedules([...schedules, data])
-                setNewTaskDate()
+                setNewTaskDate('')
+                setNewTaskName()
             })
     }
 
