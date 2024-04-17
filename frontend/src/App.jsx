bea3881740fb42857cda5d34f736a3ed16a8cc57
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-16 23:25:54 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,7 +62,7 @@ function App() {
 
     return (
         <div>
-            <button style={{position: 'absolute', top: 10, }}
+            <button style={{position: 'absolute', top: 10, right: 10}}
           <h1>Schedule</h1>
           <ul>
               {schedules.map(schedule => (
