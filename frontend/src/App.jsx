c1a6dd427a1a229340863e98f83c6482f6149f38
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-16 23:24:39 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
             document.body.classList.remove('light-mode')
         }else {
             document.body.classList.add('light-mode')
+            document.body.classList.remove('dark-mode')
         }
     })
 
