9fbbe908d998e2fcbba0fafaea31513b95b347f7
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 11:28:28 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './index.css'
-import ScheduleList from './services/ScheduleList.js'
+import ScheduleList from './services/ScheduleList.jsx'
 import ScheduleForm from './services/ScheduleForm.jsx'
 
 function App() {
