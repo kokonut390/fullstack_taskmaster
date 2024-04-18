e4a117b61192169be10802f7d499a96f868bd439
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 14:52:36 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,94 +1,15 @@
-import {useState, useEffect } from "react";
-import Task from "./components/Task";
-import taskService from './services/tasks'
-import Notification from "./components/Notification.jsx";
+import { useState, useEffect } from 'react'
+import ScheduleList from './services/ScheduleList.jsx'
+import ScheduleForm from './services/ScheduleForm.jsx'
 
-const App = () => {
-    const [tasks, setTasks] = useState([])
-    const [newTask, setNewTask] = useState('')
-    const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState('some error happened...')
-
-    useEffect(() => {
-        taskService
-            .getAll()
-            .then(initialTasks => {
-                setTasks(initialTasks)
-            })
-    }, [])
-
-    console.log('rendered', tasks.length, 'tasks')
-
-    const addTask = (event) => {
-        event.preventDefault()
-        const taskObject = {
-            content: newTask,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5,
-        }
-
-        taskService
-            .create(taskObject)
-            .then(returnedTask => {
-                setTasks(tasks.concat(returnedTask))
-                setNewTask('')
-            })
-    }
-
-    const handleTaskChange = (event) => {
-        console.log(event.target.value)
-        setNewTask(event.target.value)
-    }
-
-    const toggleImportanceOf = (id) => {
-        const task = tasks.find(t => t.id === id)
-        const changedTask = { ...task, important: !task.important }
-
-        taskService
-            .update(id, changedTask)
-            .then(returnedTask => {
-                setTasks(tasks.map(task => task.id !== id ? task : returnedTask))
-            })
-            .catch(error => {
-                setErrorMessage(
-                    `Task '${task.content}' was already deleted from server`
-                )
-                setTimeout(() => {
-                    setErrorMessage(null)
-                }, 5000)
-                setTasks(tasks.filter(t => t.id !== id))
-            })
-    }
-
-    const tasksToShow = showAll? tasks: tasks.filter(task => task.important)
-
+function App() {
     return (
         <div>
-            <h1>Tasks</h1>
-            <Notification message={errorMessage} />
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll? 'important': 'all'}
-                </button>
-            </div>
-            <ul>
-                {tasksToShow.map(task =>
-                    <Task
-                        key={task.id}
-                        task={task}
-                        toggleImportance={() => toggleImportanceOf(task.id)}
-                    />
-                )}
-            </ul>
-            <form onSubmit={addTask}>
-                <input
-                    value={newTask}
-                    onChange={handleTaskChange}
-                />
-                <button type="submit">save</button>
-            </form>
+            <h1>Schedule Manager</h1>
+            <ScheduleForm />
+            <ScheduleList />
         </div>
-    )
+    );
 }
 
 export default App
