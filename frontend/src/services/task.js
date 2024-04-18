514d9e5066f6dc16741f11e8087007c5dddba409
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 14:52:06 -0700

    Auto-commit JS on save for 227

--- frontend/src/services/task.js
+++ frontend/src/services/task.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/tasks'
+const baseUrl = 'http://localhost:3001/schedule'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
