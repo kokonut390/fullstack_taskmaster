a8ef50b6e1594f20ae0782e8f39da662e2f5f5bd
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 14:52:20 -0700

    Auto-commit JS on save for 227

--- frontend/src/services/task.js
+++ frontend/src/services/task.js
@@ -1,94 +1,25 @@
-import {useState, useEffect } from "react";
-import Task from "./components/Task";
-import taskService from './services/tasks'
-import Notification from "./components/Notification.jsx";
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/schedule'
 
-const App = () => {
-    const [tasks, setTasks] = useState([])
-    const [newTask, setNewTask] = useState('')
-    const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState('some error happened...')
-
-    useEffect(() => {
-        taskService
-            .getAll()
-            .then(initialTasks => {
-                setTasks(initialTasks)
-            })
-    }, [])
-
-    console.log('rendered', tasks.length, 'tasks')
-
-    const addTask = (event) => {
-        event.preventDefault()
-        const taskObject = {
-            content: newTask,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5,
-        }
-
-        taskService
-            .create(taskObject)
-            .then(returnedTask => {
-                setTasks(tasks.concat(returnedTask))
-                setNewTask('')
-            })
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    const nonExisting = {
+        id: 10000,
+        content: 'This task is non-existent on the server. It is misinformation.',
+        date: '3127-01-15T17:30:31.098Z',
+        important: true,
     }
-
-    const handleTaskChange = (event) => {
-        console.log(event.target.value)
-        setNewTask(event.target.value)
-    }
-
-    const toggleImportanceOf = (id) => {
-        const task = tasks.find(t => t.id === id)
-        const changedTask = { ...task, important: !task.important }
-
-        taskService
-            .update(id, changedTask)
-            .then(returnedTask => {
-                setTasks(tasks.map(task => task.id !== id ? task : returnedTask))
-            })
-            .catch(error => {
-                setErrorMessage(
-                    `Task '${task.content}' was already deleted from server`
-                )
-                setTimeout(() => {
-                    setErrorMessage(null)
-                }, 5000)
-                setTasks(tasks.filter(t => t.id !== id))
-            })
-    }
-
-    const tasksToShow = showAll? tasks: tasks.filter(task => task.important)
-
-    return (
-        <div>
-            <h1>Tasks</h1>
-            <Notification message={errorMessage} />
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll? 'important': 'all'}
-                </button>
-            </div>
-            <ul>
-                {tasksToShow.map(task =>
-                    <Task
-                        key={task.id}
-                        task={task}
-                        toggleImportance={() => toggleImportanceOf(task.id)}
-                    />
-                )}
-            </ul>
-            <form onSubmit={addTask}>
-                <input
-                    value={newTask}
-                    onChange={handleTaskChange}
-                />
-                <button type="submit">save</button>
-            </form>
-        </div>
-    )
+    return request.then(response => response.data.concat(nonExisting))
 }
 
-export default App
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, update }
