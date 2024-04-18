681cb1ad1024758c721b8811fa72d23be0c21f63
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 16:09:57 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/ScheduleForm.jsx
+++ frontend/src/services/ScheduleForm.jsx
@@ -20,7 +20,7 @@ function ScheduleForm(props) {
             props.fetchSchedules()
         } catch (error) {
             if (error.response && error.response.status === 400){
-                setError()
+                setError(error.)
             }
             console.error('Error adding/updating schedule:', error);
         }
