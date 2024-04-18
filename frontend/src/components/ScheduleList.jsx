c165e898f051b4388b97439f9cddac027d248584
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 00:22:38 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/components/ScheduleList.jsx
+++ frontend/src/components/ScheduleList.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import scheduleService from "../.";
+import scheduleService from "../services";
 
 const ScheduleList = ({scheduleId}) => {
     const [schedules, setSchedules] = useState([]);
