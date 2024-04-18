0ae0a96bcba9c92867fb38194de6c3355fcce150
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 00:22:06 -0700

    Auto-commit JSX on save for Comp 227

---
+++ frontend/components/ScheduleList.jsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from "react";
+import scheduleService from "../services/task.js";
+
+const ScheduleList = ({cityId}) => {
+    const [schedules, setSchedules] = useState([]);
+
+    useEffect(() => {
+        if (scheduleId) {
+            scheduleService.getAllSchedules(scheduleId).then(response => {
+                console.log("Schedules data received:", response);
+                setSchedules(response || []);
+            }).catch(error => {
+                console.error("Error fetching schedule", error);
+                setSchedules([]);
+            });
+        }
+    }, [scheduleID]);
+
+    if (!schedules || schedules.length === 0) {
+        return <p>No schedules.</p>;
+    }
+
+
+    return (
+        <div>
+            <h2>Schedule List</h2>
+            <ul>
+                {schedules.map(schedule =>(
+                    <li key={schedule._id}>{schedule.name} - {schedule.type}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ScheduleList;
