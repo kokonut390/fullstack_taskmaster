35a04dbb11d0f1f9ebc4c9dc50f491b8a0cddafd
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 13:15:36 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/ScheduleList.jsx
+++ frontend/src/services/ScheduleList.jsx
@@ -10,8 +10,13 @@ function ScheduleList() {
     }, []);
 
     const fetchSchedules = async () => {
-        const request = await axios.get(baseUrl)
-        return request.data
+        try {
+            const response = await axios.get(baseUrl);
+            console.log("Fetched schedules:", response.data)
+            setSchedules(response.data);
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
     };
 
     const deleteSchedule = async (id) => {
