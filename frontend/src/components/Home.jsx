aa2669736e3e21c00015d7f5680aef105ec8981e
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 01:24:39 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
     useEffect(() => {
         const fetchSchedules = async () => {
             const data = await scheduleService.getAllSchedules()
+            setSchedules()
         }
     }, []);
 }
