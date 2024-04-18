d0ecb58ba41d2f747c123f231f52eb4242b2d44e
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 11:25:17 -0700

    Auto-commit JS on save for 227

--- frontend/src/services/ScheduleList.js
+++ frontend/src/services/ScheduleList.js
@@ -5,17 +5,17 @@ function ScheduleList() {
     const [schedules, setSchedules] = useState([]);
 
     useEffect(() => {
+        const fetchSchedules = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/schedule');
+                setSchedules(response.data);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
+        };
+
         fetchSchedules();
     }, []);
-
-    const fetchSchedules = async () => {
-        try {
-            const response = await axios.get('http://localhost:3001/schedule');
-            setSchedules(response.data);
-        } catch (error) {
-            console.error('Error fetching data: ', error);
-        }
-    };
 
     return (
         <div>
