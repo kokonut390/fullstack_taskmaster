84f743be3216f16b18942bc26737fea25aa1dc4d
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 00:23:08 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import './index.css'
-import Scheduletask from '/services/tasks.js'
 
 function App() {
     const [schedules, setSchedules] = useState([])
