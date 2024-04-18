8b147c127330d07a0ec3637eaf6f50757acc654f
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 11:59:17 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/ScheduleList.jsx
+++ frontend/src/services/ScheduleList.jsx
@@ -25,7 +25,7 @@ function ScheduleList() {
                 console.log('Schedule deleted', response.data)
             }
         }catch (error){
-            
+            console.error()
         }
     }
 
