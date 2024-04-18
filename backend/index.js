eba71775537cd2c88029aa6f4a0a22e1fcc26595
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 14:54:54 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ app.post('/schedule', async (req, res) => {
 });
 
 app.put('/schedule', async (req, res) => {
-    const {id, name, date} =req.body
+    const {name, date} =req.body
     try{
         const updatedSchedule = await Schedule.findOneAndUpdate(
             {id: id},
