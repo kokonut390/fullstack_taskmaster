85523bd900c91a5a3f201fb8c824368e4037729f
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 01:34:44 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
 
     const deleteTask = (id) => {
         if (window.confirm("Are you sure you want to delete this schedule?")){
-            fetch('https://final-liangyu.onrender.com/schedule', {
+            fetch('/schedule', {
                 method: 'DELETE'
             })
                 .then(response => response.json())
