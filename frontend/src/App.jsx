3b50ba862de0f3337292cd3bfd4cf705a283df6a
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-18 15:54:13 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <div>
             <h1>Schedule Manager</h1>
             <ScheduleForm fetchSchedules={fetchSchedules}/>
-            <ScheduleList schedules={}/>
+            <ScheduleList schedules={schedules}/>
         </div>
     );
 }
