2581dc6b75928d1a67775d56cc08221ef6221db0
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-24 00:03:22 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -2,6 +2,21 @@ import React, {useState} from "react";
 import axios from "axios";
 const baseUrl = `http://localhost:3001/availability`
 
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    // Get first day of year
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    const weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return weekNo;
+}
+
+
 function AvailabilityForm ({userId}){
     const [weekNumber, setWeekNumber] = useState()
     const [availableSlots, setAvailableSlots] = useState([])
