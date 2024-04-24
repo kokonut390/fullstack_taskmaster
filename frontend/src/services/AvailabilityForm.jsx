4121d3baa030eaf8bb70a85aa3525ef63745f35d
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-23 23:44:11 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseUrl = `http://localhost:3001/schedule`
 
 function AvailabilityForm ({userId}){
-    const [week, setWeek] = useState(new Date().getWeekNumber)
+    const [weekN, setWeek] = useState(new Date().getWeekNumber)
     const [days, setDays] = useState([])
 
     const handleSubmit = async (event) => {
