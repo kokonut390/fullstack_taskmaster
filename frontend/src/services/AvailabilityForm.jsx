b2baeb571bab0c3be4c2307a0b54cffc8c08f665
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-23 23:30:07 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import {useState} from "react";
 import axios from "axios";
 
 function AvailabilityForm ({userId}){
-    
+    const [, set] = useState()
 }
