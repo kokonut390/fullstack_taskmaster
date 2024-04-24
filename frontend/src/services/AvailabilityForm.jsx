de8a1bb0ebb639277659378132ba8b81488b15b5
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-24 00:05:26 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -30,7 +30,7 @@ function AvailabilityForm ({userId}){
     }
     return(
         <form onSubmit={handleSubmit}>
-            <h2
+            <h2>Set Availability</h2>
             <button type="submit">Submit</button>
         </form>
     )
