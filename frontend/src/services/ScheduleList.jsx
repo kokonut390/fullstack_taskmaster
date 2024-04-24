d69d2ed94ef01746b0b18be8ba22805111c4d6e4
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-23 23:09:15 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/ScheduleList.jsx
+++ frontend/src/services/ScheduleList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-const baseUrl = `/api/schedule`
+const baseUrl = `https://final-liangyu.onrender.com/schedule`
 /*
 const baseUrl = `http://localhost:3001/schedule`
 */
