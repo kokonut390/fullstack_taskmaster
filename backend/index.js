b693279bb50bbff0efb069b37860dcb92bae5222
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-23 23:23:15 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -113,7 +113,7 @@
 app.get('/availability/:userId', async (req, res) => {
     try {
         const {userId} = req.params
-        const availability = await Availability
+        const availability = await Availability.find
     }
 })
 
