a54111489ef5b7c8f2bd868b4f4b69e1b1013952
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-23 22:44:52 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/ScheduleList.jsx
+++ frontend/src/services/ScheduleList.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = `http://localhost:3001/schedule`
 /*
 const baseUrl = `http://localhost:3001/schedule`
-
+*/
 function ScheduleList({ schedules, fetchSchedules}) {
     const handleDelete = async (_id) => {
         try {
