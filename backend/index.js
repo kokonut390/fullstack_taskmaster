bfb3781e128221cc11d92ef2719798fc03528870
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-23 23:19:43 -0700

    Auto-commit JS on save for 227

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@
     const Schedule = mongoose.model('Schedule', scheduleSchema);
 
     const availabilitySchema = new mongoose.Schema({
-        
+        week:
     })
 
     app.use(express.json());
