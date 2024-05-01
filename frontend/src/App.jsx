5fd447fddccc55c054d39ea26c028698d91ca152
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-30 22:33:38 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     useEffect(() => {
         const fetchAvailability = () => {
             try {
-                const response = aw
+                const response = 
             }
         }
     }, []);
