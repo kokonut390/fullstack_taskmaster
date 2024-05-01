77a01b2f69bb446696e2f9474d028b7e6fba86be
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-30 21:44:10 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -58,6 +58,8 @@ function AvailabilityForm (){
                     const end2 = new Date(`01/01/2020 ${slots[j].endTime}`)
 
                     if (start1 < end2 && start2 < end1 ){
+                        const overlapStart = new Date(Math.max(start1.getTime(), start2.getTime())).toLocaleTimeString()
+                        const overlapEnd = new Date(Math.min(end1.getTime(), end2.getTime())).toLocaleTimeString()
                         
                     }
                 }
