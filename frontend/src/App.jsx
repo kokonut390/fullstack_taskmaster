d4a357de8f3e581d4b1b9470d762a0e1bd374728
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-04-30 22:36:42 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import ScheduleList from './services/ScheduleList.jsx'
 import ScheduleForm from './services/ScheduleForm.jsx'
 import AvailabilityForm from './services/AvailabilityForm.jsx'
-const baseUrl = `https://final-liangyu.onrender.com`
+const baseUrl = `https://final-liangyu.onrender.com/schedule`
 import axios from "axios";
 import './index.css'
 
