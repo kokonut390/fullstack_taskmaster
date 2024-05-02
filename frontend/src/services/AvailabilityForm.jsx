04cb1a44320291adaba5cb386fde6c516501c1be
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-05-02 03:32:42 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -100,7 +100,7 @@ function AvailabilityForm ({initialSlots = [], fetchAvailability}){
         const formattedName = inputName
             .toLowerCase()
             .split(' ')
-            .map()
+            .map(word => )
     }
 
     return(
