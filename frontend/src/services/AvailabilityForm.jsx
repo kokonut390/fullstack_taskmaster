ef1509d2c8c853fe052aa799a97731f32228479e
Liang-Yu Ko <144297233+kokonut390@users.noreply.github.com>
2024-05-02 02:33:46 -0700

    Auto-commit JSX on save for Comp 227

--- frontend/src/services/AvailabilityForm.jsx
+++ frontend/src/services/AvailabilityForm.jsx
@@ -51,7 +51,14 @@ function AvailabilityForm ({initialSlots = [], fetchAvailability}){
             console.error('Error updating availability:', err)
         }
     }
-    
+    const calculateOverlaps = (availability) => {
+        let allSlots = [];
+        Object.values(availability).forEach(slots => {
+            allSlots = allSlots.concat(slots);
+        });
+        const newOverlaps = findOverlappingSlots(allSlots);
+        setOverlaps(newOverlaps);
+    }
 
     const findOverlappingSlots = (slots) => {
         let overlaps = []
